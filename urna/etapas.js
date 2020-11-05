fd25b06d032474688234b1cb9e703cd4b3c99c8c
Ícaro Cruz <68250279+icaro911@users.noreply.github.com>
2020-11-05 15:21:10 -0300

    Add files via upload

---
+++ urna/etapas.js
@@ -0,0 +1,57 @@
+let etapas = [
+	{
+		titulo: "VEREADOR",
+		numeros: 5,
+		candidatos: [
+			{
+				numero: '38111',
+				nome: 'Gaguinho',
+				partido: 'Roça',
+				fotos: [
+					{url:'edgard.jpg', legenda: 'Vereador'}
+				]
+			},
+			{
+				numero: '72222',
+				nome: 'Pepe Le Pew',
+				partido: "Je t'aime",
+				fotos: [
+					{url:'pepelepew.jpg', legenda: 'Vereador'}
+				]
+			},
+		]
+		
+	},
+	{
+		titulo: "PREFEITO",
+		numeros: 2,
+		candidatos: [
+			{
+				numero: '99',
+				nome: 'Eufrasino',
+				partido: 'Bang-Bang',
+				vice: 'Hortelino',
+				fotos: [
+					{url:'eu.jpg', legenda: 'Prefeito'},
+					{url:'eu2.jpg', legenda: 'Vice-Prefeito', small: true}
+				]
+				
+			},
+			{
+				numero: '84',
+				nome: 'Pernalonga',
+				partido: 'QQA-velinho',
+				vice: 'Patolino',
+				fotos: [
+					{url:'perna.jpg', legenda: 'Prefeito'},
+					{url:'pato.jpg', legenda: 'Vice-Prefeito', small: true}
+				]
+				
+			},
+		
+		]
+		
+		
+	}
+
+];
